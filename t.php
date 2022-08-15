590f68c6230140b3737ecf728cc2231fe5380384
Frank Ruan <40315138+orange2008@users.noreply.github.com>
2022-08-15 23:11:47 +0800

    minor fixes

--- t.php
+++ t.php
@@ -8,17 +8,16 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 // Get text from MySQL via ID
-$sql = "SELECT text FROM pastebin WHERE id = '$id'";
+$sql = "SELECT text FROM pastes WHERE id = '$id'";
 $result = mysqli_query($conn, $sql);
+// Output the result
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
         echo $row["text"];
     }
 } else {
-    echo "404 not found";
-    // Raise HTTP 404 error
-    header("HTTP/2 404 Not Found");
+    echo "0 results";
 }
 
 ?>
